cf9eab5800706459d1d4878e99a5d77a4c16777c
Kenneth Lau <ken@Kenneths-MBP.LauFamily.com>
2019-11-08 01:39:58 -0800

    Optimze and locate eyes on top half

--- eyeDetect.py
+++ eyeDetect.py
@@ -15,14 +15,14 @@ while True:
 
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = img[y:y+h, x:x+w]
+        roi_gray = gray[y:y+int(h/2), x:x+w]
+        roi_color = img[y:y+int(h/2), x:x+w]
 
         eyes = eye_cascade.detectMultiScale(roi_gray)
         for (ex, ey, ew, eh) in eyes:
             cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
 
-    cv2.imshow('img', img)
+    cv2.imshow('Eye Detect', img)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
